45e4553a762074cd2d09806d2dacb1e8545b363e
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-05-28 16:08:18 +0300

    Implement dynamic bookmark display and update saveBookmark function

--- JS/script.js
+++ JS/script.js
@@ -6,11 +6,17 @@ var bookmark = {
     url: bookmarkUrl
 }
 
-var bookmarkList;
+var bookmarkList= [];
 
 
 function saveBookmark() {
-    if (localStorage.getItem("bookmarkList") === null) {
+    // Create a new bookmark object with the current input values
+    var bookmark = {
+        name: bookmarkName.value,
+        url: bookmarkUrl.value
+    };
+
+    if (localStorage.getItem("bookmarkList") == null) {
         bookmarkList = [];
         bookmarkList.push(bookmark);
         localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
@@ -21,8 +27,36 @@ function saveBookmark() {
         // Update the localStorage with the new bookmark list
         localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
     }
-    console.log("$bookmarkName: " + bookmark.name.value);
-    console.log("$bookmarkUrl: " + bookmark.url.value);
+    console.log(bookmarkList);
 
+    displayBookmarks();
+}
+function displayBookmarks() {
+    if (localStorage.getItem("bookmarkList") !== null) {
+        bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+        var bookmarksResults = document.getElementById("bookmarkList");
+        bookmarksResults.innerHTML = "";
+        for (var i = 0; i < bookmarkList.length; i++) {
+            var name = bookmarkList[i].name;
+            var url = bookmarkList[i].url;
 
+            bookmarksResults.innerHTML += `<tr>
+            <td>${i+1}</td>
+            <td>${name}</td>
+            <td><button class="btn-visit" data-url="${url}" onclick="visit(${i})"><i class="fas fa-eye"></i> Visit</button></td>
+            <td><button class="btn-update" data-index="${i}"><i class="fas fa-edit"></i> Update</button></td>
+            <td><button class="btn-delete" data-index="${i}" onclick="deleteBookmark(${i})"><i class="fas fa-trash"></i> Delete</button></td>
+          </tr>`
+        }
+    }
+}
+displayBookmarks();
+function deleteBookmark(index) {
+    bookmarkList.splice(index, 1);
+    localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+    displayBookmarks();
+}
+function visit(index) {
+    var url = bookmarkList[index].url;
+    window.open(url, '_blank');
 }
