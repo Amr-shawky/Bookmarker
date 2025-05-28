bb0dab5172f44adc088a0cda57c440c5dfe2cdcf
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-05-28 19:55:54 +0300

    Refactor code structure for improved readability and maintainability

--- JS/script.js
+++ JS/script.js
@@ -8,8 +8,31 @@ var bookmark = {
 }
 
 var bookmarkList= [];
-
-
+var viewstyle = "list"; // Default view style
+function toggleViewStyle() {
+var items = document.getElementById("items");
+if (viewstyle == "list") {
+    items.innerHTML = `<table id="bookmarkTable" class="table">
+            <thead>
+              <tr>
+                <th>Index</th>
+                <th>Website Name</th>
+                <th>Visit</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody id="bookmarkList">
+    
+            </tbody>
+          </table>`;
+}
+else {
+    items.innerHTML =`<div id="bookmarkList"class="row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-1 g-4">
+    </div>`;
+}
+}
+toggleViewStyle(); // Initialize the view style
 function saveBookmark() {
     // Create a new bookmark object with the current input values
     var bookmark = {
@@ -39,19 +62,40 @@ function saveBookmark() {
 function displayBookmarks() {
     if (localStorage.getItem("bookmarkList") !== null) {
         bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
-        var bookmarksResults = document.getElementById("bookmarkList");
-        bookmarksResults.innerHTML = "";
+        toggleViewStyle(); // Initialize the view style
+                        var bookmarksResults = document.getElementById("bookmarkList");
         for (var i = 0; i < bookmarkList.length; i++) {
             var name = bookmarkList[i].name;
             var url = bookmarkList[i].url;
+            if(viewstyle == "list"){
+                bookmarksResults.innerHTML += `<tr>
+                <td>${i+1}</td>
+                <td>${name}</td>
+                <td><button class="btn-visit" data-url="${url}" onclick="visit(${i})"><i class="fas fa-eye"></i> Visit</button></td>
+                <td><button class="btn-update" data-index="${i}"onclick="update(${i})"><i class="fas fa-edit"></i> Update</button></td>
+                <td><button class="btn-delete" data-index="${i}" onclick="deleteBookmark(${i})"><i class="fas fa-trash"></i> Delete</button></td>
+              </tr>`
+            }
+            else{
+                bookmarksResults.innerHTML += `                    <div class="col">
+                        <div class="card">
+                            <img src="./Assets/Images/img4.jpeg" class="card-img-top w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                                <div class="btns d-flex justify-content-start flex-nowrap" style="height: 55px;">
+                                    <button class="btn-visit" data-url="${url}" onclick="visit(${i})"><i
+                                            class="fas fa-eye"></i> Visit</button>
+                                    <button class="btn-update" data-index="${i}" onclick="update(${i})"><i
+                                            class="fas fa-edit"></i> Update</button>
+                                    <button class="btn-delete" data-index="${i}" onclick="deleteBookmark(${i})"><i
+                                            class="fas fa-trash"></i> Delete</button>
+                                </div>
 
-            bookmarksResults.innerHTML += `<tr>
-            <td>${i+1}</td>
-            <td>${name}</td>
-            <td><button class="btn-visit" data-url="${url}" onclick="visit(${i})"><i class="fas fa-eye"></i> Visit</button></td>
-            <td><button class="btn-update" data-index="${i}"onclick="update(${i})"><i class="fas fa-edit"></i> Update</button></td>
-            <td><button class="btn-delete" data-index="${i}" onclick="deleteBookmark(${i})"><i class="fas fa-trash"></i> Delete</button></td>
-          </tr>`
+                            </div>
+                        </div>
+                    </div>`;
+            }
+            
         }
     }
     else {
@@ -98,5 +142,24 @@ function updateBookmark() {
     updateButton.style.display = "none";
     var saveButton = document.getElementById("submitBookmark");
     saveButton.style.display = "inline-block";
+    displayBookmarks();
+}
+
+function gridstyle() {
+    viewstyle = "grid";
+    var gridbutton = document.getElementById("gridstyle");
+    gridbutton.classList.add("active");
+    var listbutton = document.getElementById("liststyle");
+    listbutton.classList.remove("active");
+    toggleViewStyle();
+    displayBookmarks();
+}
+function liststyle() {
+    viewstyle = "list";
+    var listbutton = document.getElementById("liststyle");
+    listbutton.classList.add("active");
+    var gridbutton = document.getElementById("gridstyle");
+    gridbutton.classList.remove("active");
+    toggleViewStyle();
     displayBookmarks();
 }
